fca62b89b1c7f2c308bda41976f5492f80e68235
Raphael Pereira <raphael.pereira@grupogen.com.br>
2025-03-13 12:53:53 -0300

    :bug:  remove o aws_s3_bucket_encryption que adicionei sem verificar se o provider hashicorp/aws existia

--- main.tf
+++ main.tf
@@ -17,18 +17,6 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
-resource "aws_s3_bucket_encryption" "encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.terraform_state.id
 
